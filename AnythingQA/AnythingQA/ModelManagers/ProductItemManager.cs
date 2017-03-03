f72e58b43efc5e59903b0aafdbe11419621c7600
Isham Mohamed <IshamMohamed@users.noreply.github.com>
2017-03-03 09:56:26 +0530

    Update ProductItemManager.cs
    Modded to function as a common manager for all Tables

--- AnythingQA/AnythingQA/ModelManagers/ProductItemManager.cs
+++ AnythingQA/AnythingQA/ModelManagers/ProductItemManager.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * To add Offline Sync Support:
  *  1) Add the NuGet package Microsoft.Azure.Mobile.Client.SQLiteStore (and dependencies) to all client projects
  *  2) Uncomment the #define OFFLINE_SYNC_ENABLED
@@ -32,8 +32,18 @@ namespace AnythingQA.ModelManagers
 
 #if OFFLINE_SYNC_ENABLED
         IMobileServiceSyncTable<ProductItem> productTable;
+		IMobileServiceSyncTable<Cart> cartTable;
+		IMobileServiceSyncTable<CartItem> cartItemTable;
+		IMobileServiceSyncTable<Customer> customerTable;
+		IMobileServiceSyncTable<Merchant> merchantTable;
+		IMobileServiceSyncTable<Order> orderTable;
 #else
         IMobileServiceTable<ProductItem> productTable;
+		IMobileServiceTable<Cart> cartTable;
+		IMobileServiceTable<CartItem> cartItemTable;
+		IMobileServiceTable<Customer> customerTable;
+		IMobileServiceTable<Merchant> merchantTable;
+		IMobileServiceTable<Order> orderTable;
 
 #endif
 
@@ -66,6 +76,7 @@ namespace AnythingQA.ModelManagers
             get { return productTable is Microsoft.WindowsAzure.MobileServices.Sync.IMobileServiceSyncTable<ProductItem>; }
         }
 
+		#region ProductItem
         public async Task<ObservableCollection<ProductItem>> GetProductItemAsync(bool syncItems = false)
         {
             try
@@ -73,7 +84,7 @@ namespace AnythingQA.ModelManagers
 #if OFFLINE_SYNC_ENABLED
                 if (syncItems)
                 {
-                    await this.SyncAsync();
+                    await this.SyncProductItemAsync();
                 }
 #endif
                 IEnumerable<ProductItem> items = await productTable.ToEnumerableAsync();
@@ -90,7 +101,7 @@ namespace AnythingQA.ModelManagers
             return null;
         }
 
-        public async Task SaveTaskAsync(ProductItem item)
+        public async Task SaveProductItemAsync(ProductItem item)
         {
             if (item.Id == null)
             {
@@ -103,7 +114,7 @@ namespace AnythingQA.ModelManagers
         }
 
 #if OFFLINE_SYNC_ENABLED
-        public async Task SyncAsync()
+        public async Task SyncProductItemAsync()
         {
             ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
 
@@ -147,5 +158,426 @@ namespace AnythingQA.ModelManagers
             }
         }
 #endif
-    }
+		#endregion
+		
+		#region Cart
+        public async Task<ObservableCollection<Cart>> GetCartAsync(bool syncItems = false)
+        {
+            try
+            {
+#if OFFLINE_SYNC_ENABLED
+                if (syncItems)
+                {
+                    await this.SyncCartAsync();
+                }
+#endif
+                IEnumerable<Cart> items = await cartTable.ToEnumerableAsync();
+                return new ObservableCollection<Cart>(items);
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine(@"Invalid sync operation: {0}", msioe.Message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(@"Sync error: {0}", e.Message);
+            }
+            return null;
+        }
+
+        public async Task SaveCartAsync(Cart item)
+        {
+            if (item.Id == null)
+            {
+                await cartTable.InsertAsync(item);
+            }
+            else
+            {
+                await cartTable.UndeleteAsync(item);
+            }
+        }
+
+#if OFFLINE_SYNC_ENABLED
+        public async Task SyncCartAsync()
+        {
+            ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
+
+            try
+            {
+                await this.client.SyncContext.PushAsync();
+
+                await this.cartTable.PullAsync(
+                    //The first parameter is a query name that is used internally by the client SDK to implement incremental sync.
+                    //Use a different query name for each unique query in your program
+                    "allCarts",
+                    this.cartTable.CreateQuery());
+            }
+            catch (MobileServicePushFailedException exc)
+            {
+                if (exc.PushResult != null)
+                {
+                    syncErrors = exc.PushResult.Errors;
+                }
+            }
+
+            // Simple error/conflict handling. A real application would handle the various errors like network conditions,
+            // server conflicts and others via the IMobileServiceSyncHandler.
+            if (syncErrors != null)
+            {
+                foreach (var error in syncErrors)
+                {
+                    if (error.OperationKind == MobileServiceTableOperationKind.Update && error.Result != null)
+                    {
+                        //Update failed, reverting to server's copy.
+                        await error.CancelAndUpdateItemAsync(error.Result);
+                    }
+                    else
+                    {
+                        // Discard local change.
+                        await error.CancelAndDiscardItemAsync();
+                    }
+
+                    Debug.WriteLine(@"Error executing sync operation. Item: {0} ({1}). Operation discarded.", error.TableName, error.Item["id"]);
+                }
+            }
+        }
+#endif
+		#endregion
+    
+		#region CartItem
+        public async Task<ObservableCollection<CartItem>> GetCartItemAsync(bool syncItems = false)
+        {
+            try
+            {
+#if OFFLINE_SYNC_ENABLED
+                if (syncItems)
+                {
+                    await this.SyncCartItemAsync();
+                }
+#endif
+                IEnumerable<CartItem> items = await cartItemTable.ToEnumerableAsync();
+                return new ObservableCollection<CartItem>(items);
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine(@"Invalid sync operation: {0}", msioe.Message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(@"Sync error: {0}", e.Message);
+            }
+            return null;
+        }
+
+        public async Task SaveCartItemAsync(CartItem item)
+        {
+            if (item.Id == null)
+            {
+                await cartItemTable.InsertAsync(item);
+            }
+            else
+            {
+                await cartItemTable.UndeleteAsync(item);
+            }
+        }
+
+#if OFFLINE_SYNC_ENABLED
+        public async Task SyncCartItemAsync()
+        {
+            ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
+
+            try
+            {
+                await this.client.SyncContext.PushAsync();
+
+                await this.cartItemTable.PullAsync(
+                    //The first parameter is a query name that is used internally by the client SDK to implement incremental sync.
+                    //Use a different query name for each unique query in your program
+                    "allCartItems",
+                    this.cartItemTable.CreateQuery());
+            }
+            catch (MobileServicePushFailedException exc)
+            {
+                if (exc.PushResult != null)
+                {
+                    syncErrors = exc.PushResult.Errors;
+                }
+            }
+
+            // Simple error/conflict handling. A real application would handle the various errors like network conditions,
+            // server conflicts and others via the IMobileServiceSyncHandler.
+            if (syncErrors != null)
+            {
+                foreach (var error in syncErrors)
+                {
+                    if (error.OperationKind == MobileServiceTableOperationKind.Update && error.Result != null)
+                    {
+                        //Update failed, reverting to server's copy.
+                        await error.CancelAndUpdateItemAsync(error.Result);
+                    }
+                    else
+                    {
+                        // Discard local change.
+                        await error.CancelAndDiscardItemAsync();
+                    }
+
+                    Debug.WriteLine(@"Error executing sync operation. Item: {0} ({1}). Operation discarded.", error.TableName, error.Item["id"]);
+                }
+            }
+        }
+#endif
+		#endregion
+	
+		#region Customer
+        public async Task<ObservableCollection<Customer>> GetCustomerAsync(bool syncItems = false)
+        {
+            try
+            {
+#if OFFLINE_SYNC_ENABLED
+                if (syncItems)
+                {
+                    await this.SyncCustomerAsync();
+                }
+#endif
+                IEnumerable<Customer> items = await customerTable.ToEnumerableAsync();
+                return new ObservableCollection<Customer>(items);
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine(@"Invalid sync operation: {0}", msioe.Message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(@"Sync error: {0}", e.Message);
+            }
+            return null;
+        }
+
+        public async Task SaveCustomerAsync(Customer item)
+        {
+            if (item.Id == null)
+            {
+                await customerTable.InsertAsync(item);
+            }
+            else
+            {
+                await customerTable.UndeleteAsync(item);
+            }
+        }
+
+#if OFFLINE_SYNC_ENABLED
+        public async Task SyncCustomerAsync()
+        {
+            ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
+
+            try
+            {
+                await this.client.SyncContext.PushAsync();
+
+                await this.customerTable.PullAsync(
+                    //The first parameter is a query name that is used internally by the client SDK to implement incremental sync.
+                    //Use a different query name for each unique query in your program
+                    "allCustomers",
+                    this.customerTable.CreateQuery());
+            }
+            catch (MobileServicePushFailedException exc)
+            {
+                if (exc.PushResult != null)
+                {
+                    syncErrors = exc.PushResult.Errors;
+                }
+            }
+
+            // Simple error/conflict handling. A real application would handle the various errors like network conditions,
+            // server conflicts and others via the IMobileServiceSyncHandler.
+            if (syncErrors != null)
+            {
+                foreach (var error in syncErrors)
+                {
+                    if (error.OperationKind == MobileServiceTableOperationKind.Update && error.Result != null)
+                    {
+                        //Update failed, reverting to server's copy.
+                        await error.CancelAndUpdateItemAsync(error.Result);
+                    }
+                    else
+                    {
+                        // Discard local change.
+                        await error.CancelAndDiscardItemAsync();
+                    }
+
+                    Debug.WriteLine(@"Error executing sync operation. Item: {0} ({1}). Operation discarded.", error.TableName, error.Item["id"]);
+                }
+            }
+        }
+#endif
+		#endregion
+	
+		#region Merchant
+        public async Task<ObservableCollection<Merchant>> GetMerchantAsync(bool syncItems = false)
+        {
+            try
+            {
+#if OFFLINE_SYNC_ENABLED
+                if (syncItems)
+                {
+                    await this.SyncMerchantAsync();
+                }
+#endif
+                IEnumerable<Merchant> items = await merchantTable.ToEnumerableAsync();
+                return new ObservableCollection<Merchant>(items);
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine(@"Invalid sync operation: {0}", msioe.Message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(@"Sync error: {0}", e.Message);
+            }
+            return null;
+        }
+
+        public async Task SaveMerchantAsync(Merchant item)
+        {
+            if (item.Id == null)
+            {
+                await merchantTable.InsertAsync(item);
+            }
+            else
+            {
+                await merchantTable.UndeleteAsync(item);
+            }
+        }
+
+#if OFFLINE_SYNC_ENABLED
+        public async Task SyncMerchantAsync()
+        {
+            ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
+
+            try
+            {
+                await this.client.SyncContext.PushAsync();
+
+                await this.merchantTable.PullAsync(
+                    //The first parameter is a query name that is used internally by the client SDK to implement incremental sync.
+                    //Use a different query name for each unique query in your program
+                    "allMerchants",
+                    this.merchantTable.CreateQuery());
+            }
+            catch (MobileServicePushFailedException exc)
+            {
+                if (exc.PushResult != null)
+                {
+                    syncErrors = exc.PushResult.Errors;
+                }
+            }
+
+            // Simple error/conflict handling. A real application would handle the various errors like network conditions,
+            // server conflicts and others via the IMobileServiceSyncHandler.
+            if (syncErrors != null)
+            {
+                foreach (var error in syncErrors)
+                {
+                    if (error.OperationKind == MobileServiceTableOperationKind.Update && error.Result != null)
+                    {
+                        //Update failed, reverting to server's copy.
+                        await error.CancelAndUpdateItemAsync(error.Result);
+                    }
+                    else
+                    {
+                        // Discard local change.
+                        await error.CancelAndDiscardItemAsync();
+                    }
+
+                    Debug.WriteLine(@"Error executing sync operation. Item: {0} ({1}). Operation discarded.", error.TableName, error.Item["id"]);
+                }
+            }
+        }
+#endif
+		#endregion
+	
+		#region Order
+        public async Task<ObservableCollection<Order>> GetOrderAsync(bool syncItems = false)
+        {
+            try
+            {
+#if OFFLINE_SYNC_ENABLED
+                if (syncItems)
+                {
+                    await this.SyncOrderAsync();
+                }
+#endif
+                IEnumerable<Order> items = await orderTable.ToEnumerableAsync();
+                return new ObservableCollection<Order>(items);
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine(@"Invalid sync operation: {0}", msioe.Message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(@"Sync error: {0}", e.Message);
+            }
+            return null;
+        }
+
+        public async Task SaveOrderAsync(Order item)
+        {
+            if (item.Id == null)
+            {
+                await orderTable.InsertAsync(item);
+            }
+            else
+            {
+                await orderTable.UndeleteAsync(item);
+            }
+        }
+
+#if OFFLINE_SYNC_ENABLED
+        public async Task SyncOrderAsync()
+        {
+            ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
+
+            try
+            {
+                await this.client.SyncContext.PushAsync();
+
+                await this.orderTable.PullAsync(
+                    //The first parameter is a query name that is used internally by the client SDK to implement incremental sync.
+                    //Use a different query name for each unique query in your program
+                    "allOrders",
+                    this.orderTable.CreateQuery());
+            }
+            catch (MobileServicePushFailedException exc)
+            {
+                if (exc.PushResult != null)
+                {
+                    syncErrors = exc.PushResult.Errors;
+                }
+            }
+
+            // Simple error/conflict handling. A real application would handle the various errors like network conditions,
+            // server conflicts and others via the IMobileServiceSyncHandler.
+            if (syncErrors != null)
+            {
+                foreach (var error in syncErrors)
+                {
+                    if (error.OperationKind == MobileServiceTableOperationKind.Update && error.Result != null)
+                    {
+                        //Update failed, reverting to server's copy.
+                        await error.CancelAndUpdateItemAsync(error.Result);
+                    }
+                    else
+                    {
+                        // Discard local change.
+                        await error.CancelAndDiscardItemAsync();
+                    }
+
+                    Debug.WriteLine(@"Error executing sync operation. Item: {0} ({1}). Operation discarded.", error.TableName, error.Item["id"]);
+                }
+            }
+        }
+#endif
+		#endregion
+	}
 }
