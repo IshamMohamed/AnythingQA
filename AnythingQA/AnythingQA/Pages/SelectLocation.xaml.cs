00cf3526eba700e07a7f3200f4b32f65ab64e1ca
Isham Mohamed <isham4you@gmail.com>
2017-03-28 00:26:47 +0530

    Load location from already given location stored in AppData

--- AnythingQA/AnythingQA/Pages/SelectLocation.xaml.cs
+++ AnythingQA/AnythingQA/Pages/SelectLocation.xaml.cs
@@ -6,16 +6,18 @@ using Xamarin.Forms.Maps;
 
 using Plugin.Geolocator;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace AnythingQA.Pages
 {
     public partial class SelectLocation : ContentPage
     {
         Plugin.Geolocator.Abstractions.Position position;
+        Geocoder geocoder = new Geocoder();
         public SelectLocation()
         {
             InitializeComponent();
-            //PutSomePinsOnMap();
+            GetAddressLatLangFromSettings();
         }
 
         async Task PutSomePinsOnMap()
@@ -44,7 +46,7 @@ namespace AnythingQA.Pages
             try
             {
                 var locator = CrossGeolocator.Current;
-                locator.DesiredAccuracy = 1;
+                locator.DesiredAccuracy = 0.1;
                 if (!locator.IsListening)
                     await locator.StartListeningAsync(100, 1000);
                 var x = locator.IsGeolocationAvailable;
@@ -54,6 +56,12 @@ namespace AnythingQA.Pages
             {
 
             }
+            UpdateMap();
+            PrintItemsToTextBox();
+        }
+
+        private void UpdateMap()
+        {
             Pin myLocationPin = new Pin
             {
                 Label = "My Current Location",
@@ -63,17 +71,43 @@ namespace AnythingQA.Pages
             theMap.Pins.Add(myLocationPin);
             theMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude),
                                                          Distance.FromMiles(0.25)));
-            PrintItemsToTextBox();
         }
 
         private async void PrintItemsToTextBox()
         {
-            Geocoder geocoder = new Geocoder();
             var addresses = await geocoder.GetAddressesForPositionAsync(new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude));
             //foreach (var address in addresses)
             //    txtDeliveryAddress.Text += address + ",";
 
             txtDeliveryAddress.Text = addresses.FirstOrDefault();
+        }
+
+        private async void GetAddressLatLangFromSettings()
+        {
+            string address = null;
+            if (Application.Current.Properties.ContainsKey(Constants.AppDataCustomerAddressKey))
+            {
+                address = (string)Application.Current.Properties[Constants.AppDataCustomerAddressKey];
+                System.Collections.Generic.IEnumerable<Position> positions = await geocoder.GetPositionsForAddressAsync(address);
+                var xamPostion = positions.FirstOrDefault<Position>();
+                position.Latitude = xamPostion.Latitude;
+                position.Longitude = xamPostion.Longitude;
+
+                Position currPosition = new Position(position.Latitude, position.Longitude);
+                Pin[] pins =
+                {
+                    new Pin() {  Label = "Usual Location",
+                        Position = new Position(position.Latitude, position.Longitude), Type = PinType.Place }
+                };
+
+                foreach (Pin p in pins)
+                {
+                    theMap.Pins.Add(p);
+                }
+
+                // center the map on Tour Eiffel / set the zoom level
+                theMap.MoveToRegion(MapSpan.FromCenterAndRadius(currPosition, Distance.FromKilometers(2.5)));
+            }
         }
 
         async void OnButtonGetCurrrentLocClicked(object sender, EventArgs e)
